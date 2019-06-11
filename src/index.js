9b9a06622cfec2e80284d760e44ca1aa71c2d27f
Andrew Prakash <andrew.prakash@gmail.com>
2019-06-11 23:54:08 +1200

    formatted the output

--- src/index.js
+++ src/index.js
@@ -1,52 +1,63 @@
-import React, { useState, useCallback } from 'react';
-import ReactDOM from 'react-dom'
-import styled, { css } from 'styled-components';
-import { useGesture } from "react-with-gesture"
+import React, { useState, useCallback } from "react";
+import ReactDOM from "react-dom";
+import styled, { css } from "styled-components";
+import { useGesture } from "react-with-gesture";
 
-
-const root = document.querySelector("#root")
+const root = document.querySelector("#root");
 
 const Container = styled.div`
-    width: 40em;
-    margin: auto;
-    overflow: hidden;
+  width: 40em;
+  margin: auto;
+  overflow: hidden;
 `;
 
 const CarouselContainer = styled.div`
-    display: flex;
-    position: relative;
-    left: ${({ left }) => left}px;
+  display: flex;
+  position: relative;
+  left: ${({ left }) => left}px;
 `;
 
 const Card = styled.div`
-   min-width: calc(33.33% - 55px);
-    padding: 20px;
-    border: 1px solid black;
-    margin-left: 10px;
-    user-select: none;
-    &:hover {
-        cursor: pointer;
-    }
+  min-width: calc(33.33% - 55px);
+  padding: 20px;
+  border: 1px solid black;
+  margin-left: 10px;
+  user-select: none;
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 const Cart = styled.div``;
 
 const App = props => {
-    const [left, setleft] = useState(0)
-    const handleMove = useCallback(event => {
-        const { previous, xy, down } = event;
-        if (down) {
-            const diff = xy[0] - previous[0]
-            if (diff > 0) {
-                setleft(preState => preState + diff)
-            } else {
-                setleft(preState => diff + preState)
-            }
+  const [left, setleft] = useState(0);
+  const handleMove = useCallback(
+    event => {
+      const { previous, xy, down } = event;
+      if (down) {
+        const diff = xy[0] - previous[0];
+        if (diff > 0) {
+          setleft(preState => preState + diff);
+        } else {
+          setleft(preState => diff + preState);
         }
-    }, [left])
+      }
+    },
+    [left]
+  );
 
-    const bind = useGesture({ onAction: handleMove })
-    return <Container {...bind()}><CarouselContainer left={left}><Card>First</Card><Card>Second</Card><Card>Third</Card><Card>Fourth</Card></CarouselContainer></Container>
-}
+  const bind = useGesture({ onAction: handleMove });
+  return (
+    <Container {...bind()}>
+      <CarouselContainer left={left}>
+        <Card>First</Card>
+        <Card>Second</Card>
+        <Card>Third</Card>
+        <Card>Fourth</Card>
+      </CarouselContainer>
+    </Container>
+  );
+};
 
-ReactDOM.render(<App />, root)
+ReactDOM.render(<App />, root);
